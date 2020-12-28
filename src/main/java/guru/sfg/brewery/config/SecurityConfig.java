13c1e09505e71f289f05a1f027463757bfc57fa6
Pablo Casas Delgado <casdelprog@gmail.com>
2020-12-28 20:05:15 +0100

    FluentAPI Example

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -32,16 +33,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.httpBasic();
 	}
 
+	
+	
 	@Override
-	@Bean
-	protected UserDetailsService userDetailsService() {
-		UserDetails admin = User.withDefaultPasswordEncoder()
-				.username("spring").password("guru").roles("ADMIN").build();
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("user").password("password").roles("USER").build();
-		
-		return new InMemoryUserDetailsManager(admin, user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("spring")
+			.password("{noop}guru")
+			.roles("ADMIN")
+			.and()
+			.withUser("user")
+			.password("{noop}password")
+			.roles("USER");
 	}
+
+
+
+//	@Override
+//	@Bean
+//	protected UserDetailsService userDetailsService() {
+//		UserDetails admin = User.withDefaultPasswordEncoder()
+//				.username("spring").password("guru").roles("ADMIN").build();
+//		UserDetails user = User.withDefaultPasswordEncoder()
+//				.username("user").password("password").roles("USER").build();
+//		
+//		return new InMemoryUserDetailsManager(admin, user);
+//	}
 
 	
 }
