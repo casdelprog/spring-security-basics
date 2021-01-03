0bc1c5bbe0ef96e963cfd2c2aa057da64f966f8e
Pablo Casas Delgado <casdelprog@gmail.com>
2021-01-03 18:58:17 +0100

    updated tests

--- src/test/java/guru/sfg/brewery/web/controllers/IndexControllerTestIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerTestIT.java
@@ -5,10 +5,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import guru.sfg.brewery.repositories.BeerInventoryRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.services.BeerService;
+import guru.sfg.brewery.services.BreweryService;
 
 @WebMvcTest
 class IndexControllerTestIT extends BaseIT{
 
+    @MockBean
+    BeerRepository beerRepository;
+
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    BreweryService breweryService;
+
+    @MockBean
+    CustomerRepository customerRepository;
+
+    @MockBean
+    BeerService beerService;
+    
 	@Test
 	void testGetIndexSlash() throws Exception{
 		mockMvc.perform(get("/"))
