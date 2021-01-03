be09bcd1aff09a95934fef49c230458c1071b330
Pablo Casas Delgado <casdelprog@gmail.com>
2021-01-03 12:55:51 +0100

    Custom auth

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -3,16 +3,26 @@ package guru.sfg.brewery.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import guru.sfg.brewery.security.RestHeaderAuthFilter;
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	public RestHeaderAuthFilter restHeaderAuthFilter(AuthenticationManager authenticationManager) {
+		RestHeaderAuthFilter filter = new RestHeaderAuthFilter(new AntPathRequestMatcher("/api/**"));
+		filter.setAuthenticationManager(authenticationManager);
+		return filter;
+	}
+	
 	@Bean
     PasswordEncoder passwordEncoder(){	   
 		return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -20,6 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(
+				restHeaderAuthFilter(authenticationManager()), 
+				UsernamePasswordAuthenticationFilter.class)
+		.csrf().disable();
+		
 		http
 		//added to default configuration to exclude rss and login from auth
 		.authorizeRequests(auth -> {
--- src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/api/BeerRestControllerIT.java
@@ -1,5 +1,7 @@
 package guru.sfg.brewery.web.controllers.api;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,23 +11,45 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import guru.sfg.brewery.web.controllers.BaseIT;
 
 @WebMvcTest
-class BeerRestControllerIT extends BaseIT{
-	
+class BeerRestControllerIT extends BaseIT {
+
 	@Test
-	void findBeers() throws Exception{
-		mockMvc.perform(get("/api/v1/beer"))
-				.andExpect(status().isOk());
+	void deleteBeer() throws Exception {
+		mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311").header("Api-Key", "spring")
+				.header("Api-Secret", "guru")).andExpect(status().isOk());
 	}
-	
+
+    @Test
+    void deleteBeerBadCreds() throws Exception{
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .header("Api-Key","spring").header("Api-Secret", "guruXXXX"))
+                .andExpect(status().isUnauthorized());
+    }
+
 	@Test
-	void findBeersById() throws Exception{
-		mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
-				.andExpect(status().isOk());
+	void deleteBeerHttpBasic() throws Exception {	
+		mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311").with(httpBasic("spring", "guru")))
+				.andExpect(status().is2xxSuccessful());
 	}
 
 	@Test
-	void findBeersByUpc() throws Exception{
-		mockMvc.perform(get("/api/v1/beerUpc/0631234200036"))
-				.andExpect(status().isOk());
+	void deleteBeerNoAuth() throws Exception {
+		mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+				.andExpect(status().isUnauthorized());
+	}
+
+	@Test
+	void findBeers() throws Exception {
+		mockMvc.perform(get("/api/v1/beer")).andExpect(status().isOk());
+	}
+
+	@Test
+	void findBeersById() throws Exception {
+		mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")).andExpect(status().isOk());
+	}
+
+	@Test
+	void findBeersByUpc() throws Exception {
+		mockMvc.perform(get("/api/v1/beerUpc/0631234200036")).andExpect(status().isOk());
 	}
 }
